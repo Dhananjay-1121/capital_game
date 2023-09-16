2f3b3f2191323073ad243c129722eaff115511ac
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-17 02:19:46 +0530

    background correction

--- app/page.js
+++ app/page.js
@@ -5,28 +5,17 @@ export default function Home() {
   return (
     <div className="flex flex-col bg-gray-300 w-full h-screen text-31xl font-poppins">
       <Image src="/bg@2x.png" fill />
-      <div className="flex  justify-center">
-        <Nav />
-      </div>
-
-      {/* <div className=" mx-24 flex">
-        <div className="flex gap-60 text-center mx-auto">
-          <Image src="/logo.png" width={110} height={70} />
-          <div className="flex gap-14 z-30">
-            <button className="navItem">Home</button>
-            <button className="navItem">About Us</button>
-            <button className="navItem">Timeline</button>
-            <button className="navItem">Blog</button>
-          </div>
-          <button className=" text-4xl"> PLAY NOW</button>
+      <div className="bg-gradient-to-t from-black via-transparent to-slate-600 ">
+        <div className="flex  justify-center ">
+          <Nav />
         </div>
-      </div> */}
 
-      <div className="flex justify-center text-center mt-48 ">
-        <div>
-          <h2 className=" leading-tight text-white z-20">
-            LEARN HOW TO GROW YOUT MONEY WITH CAPITAL RUSH
-          </h2>
+        <div className="flex justify-center text-center mt-48 ">
+          <div>
+            <h2 className=" leading-tight text-white z-20">
+              LEARN HOW TO GROW YOUT MONEY WITH CAPITAL RUSH
+            </h2>
+          </div>
         </div>
       </div>
     </div>
