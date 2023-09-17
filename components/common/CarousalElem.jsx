5b783d18cc72cdf4787f470991b3836b38f4dd45
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-17 17:25:22 +0530

    carosel working on button click

--- components/common/CarousalElem.jsx
+++ components/common/CarousalElem.jsx
@@ -1,49 +1,57 @@
     "use client"
-    import React, { useRef, useState } from 'react';
-    // Import Swiper React components
+    import React from 'react';
     import { Swiper, SwiperSlide } from 'swiper/react';
-
-    // Import Swiper styles
-    import 'swiper/css';
-    import 'swiper/css/navigation';
-    import 'swiper/css/pagination';
-    import "./caurosal.css";
-
-
-    // import required modules
+    import 'swiper/css/bundle'; // Import Swiper styles
+    import './caurosal.css'; // Your CSS file
     import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
-import Image from 'next/image';
-
-
+    import Image from 'next/image';
+    
     const CarousalElem = () => {
-    return (
-        <div className="flex justify-center" >
-        <Swiper
-            cssMode={true}
-            navigation={true}
+      return (
+        <div className="carousel-container">
+          <div className="carousel-buttons z-30">
+            <button className="carousel-button cursor-pointer carousel-button-prev">Left</button>
+            <button className="carousel-button cursor-pointer carousel-button-next">Right</button>
+          </div>
+          <Swiper
+            navigation={{
+              prevEl: '.carousel-button-prev',
+              nextEl: '.carousel-button-next',
+            }}
             pagination={true}
-            // mousewheel={true}
-            // keyboard={true}
-            slidesPerView={4} // Show 4 slides at a time
-            // spaceBetween={30} // Add a gap of 20 pixels between slides
+            mousewheel={true}
+            keyboard={true}
+            slidesPerView={4}
             modules={[Navigation, Pagination, Mousewheel, Keyboard]}
             className="mySwiper"
-        >
-        <div className="px-12 relative">
-            <SwiperSlide> <Image src="/a2.png" className="px-5" height={100} width={100}/></SwiperSlide>
-            <SwiperSlide> <Image src="/a1.png" className="px-5" height={100} width={100}/></SwiperSlide>
-            <SwiperSlide> <Image src="/a3.png" className="px-5" height={100} width={100}/></SwiperSlide>
-            <SwiperSlide> <Image src="/a4.png" className="px-5" height={100} width={100}/></SwiperSlide>
-            <SwiperSlide> <Image src="/a1.png" className="px-5" height={100} width={100}/></SwiperSlide>
-            <SwiperSlide> <Image src="/a2.png" className="px-5" height={100} width={100}/></SwiperSlide>
+          >
+            <div className="px-12">
+              <SwiperSlide>
+                <Image src="/a2.png" fill />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/a1.png" fill />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/a3.png" fill />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/a4.png" fill />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/a1.png" fill />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image src="/a2.png" fill />
+              </SwiperSlide>
+            </div>
+          </Swiper>
         </div>
-            
-        </Swiper>
-        </div>
-    );
+      );
     };
-
+    
     export default CarousalElem;
+    
 
 
     // "use client";
