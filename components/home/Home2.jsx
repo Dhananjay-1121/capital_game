c8cbdf5a680c9809dac345e1589791df80751cf0
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-17 13:08:15 +0530

    caurosal phase 1

--- components/home/Home2.jsx
+++ components/home/Home2.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import CarousalElem from "../common/CarousalElem";
 
 const Home2 = () => {
   return (
     <section className="home-sec2">
-        Home
+      <div className="py-24">
+        <div className=" text-center">
+          <h2 className="  mx-28 leading-tight text-white text-[50px] ">
+            WHY
+            <span className=" text-yellow-300 text-stroke text-stroke-white">
+              {" "}
+              CAPITAL RUSH?
+            </span>
+          </h2>
+          <p className=" text-white -mt-5 font-quicksand leading-normal text-[20px]">
+            Transforming gaming into learning: Capital Rush merges finance
+            education <br /> and fun, perfect for gamers and investment novices
+          </p>
+        </div>
+        <div className=" mt-24 mx-32">
+            <CarousalElem />
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home2
+export default Home2;
