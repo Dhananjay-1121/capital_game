f5badf6a5d7442480e2a64385a529fe8f574a939
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-17 18:49:24 +0530

    pagination dots are at bottom of carosal

--- components/common/CarousalElem.jsx
+++ components/common/CarousalElem.jsx
@@ -1,122 +1,160 @@
-    "use client"
-    import React from 'react';
-    import { Swiper, SwiperSlide } from 'swiper/react';
-    import 'swiper/css/bundle'; // Import Swiper styles
-    import './caurosal.css'; // Your CSS file
-    import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
-    import Image from 'next/image';
-    
-    const CarousalElem = () => {
-      return (
-        <div className="carousel-container py-10 flex gap-20 items-center">
-          <div className="carousel-buttons z-30">
-            <button className="carousel-button cursor-pointer carousel-button-prev">Left</button>
-          </div>
-          <Swiper
-            navigation={{
-              prevEl: '.carousel-button-prev',
-              nextEl: '.carousel-button-next',
-            }}
-            pagination={true}
-            mousewheel={true}
-            keyboard={true}
-            slidesPerView={4}
-            spaceBetween={30}
-            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-            // loop={true}
-            className="mySwiper"
-          >
-            <div className="">
-              <SwiperSlide className="group">
-                <Image className="h-10 group-hover:brightness-125" src="/a1.png" fill />
-              </SwiperSlide>
-              <SwiperSlide className="group">
-                <Image className="h-10 group-hover:brightness-125" src="/a3.png" fill />
-              </SwiperSlide>
-              <SwiperSlide className="group">
-                <Image className="h-10 group-hover:brightness-125" src="/a4.png" fill />
-              </SwiperSlide>
-              <SwiperSlide className="group">
-                <Image className="h-10 group-hover:brightness-125" src="/a2.png" fill />
-              </SwiperSlide>
-              <SwiperSlide className="group">
-                <Image className="h-10 group-hover:brightness-125" src="/a1.png" fill />
-              </SwiperSlide>
-              <SwiperSlide className="group">
-                <Image className="h-10 group-hover:brightness-125" src="/a3.png" fill />
-              </SwiperSlide>
-              <SwiperSlide className="group">
-                <Image className="h-10 group-hover:brightness-125" src="/a4.png" fill />
-              </SwiperSlide>
-              
-            </div>
-          </Swiper>
-          <div className="carousel-buttons z-30">
-            <button className="carousel-button cursor-pointer carousel-button-next">Right</button>
-          </div>
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle"; // Import Swiper styles
+import "./caurosal.css"; // Your CSS file
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+import Image from "next/image";
+
+const CarousalElem = () => {
+  return (
+    <div className="carousel-container  flex-col items-center">
+      <div className="py-10 flex gap-20 items-center">
+        <div className="carousel-buttons z-30">
+          <button className="carousel-button cursor-pointer carousel-button-prev">
+            Left
+          </button>
         </div>
-      );
-    };
-    
-    export default CarousalElem;
-    
+        <Swiper
+          navigation={{
+            prevEl: ".carousel-button-prev",
+            nextEl: ".carousel-button-next",
+          }}
+          pagination={{
+            el: ".swiper-pagination",
+            clickable: true,
+          }}
+          // pagination={true}
+          mousewheel={true}
+          keyboard={true}
+          slidesPerView={4}
+          spaceBetween={30}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          // loop={true}
+          className="mySwiper"
+        >
+          <div className="">
+            <SwiperSlide className="group">
+              <Image
+                className="h-10 group-hover:brightness-125"
+                src="/a1.png"
+                fill
+              />
+            </SwiperSlide>
+            <SwiperSlide className="group">
+              <Image
+                className="h-10 group-hover:brightness-125"
+                src="/a3.png"
+                fill
+              />
+            </SwiperSlide>
+            <SwiperSlide className="group">
+              <Image
+                className="h-10 group-hover:brightness-125"
+                src="/a4.png"
+                fill
+              />
+            </SwiperSlide>
+            <SwiperSlide className="group">
+              <Image
+                className="h-10 group-hover:brightness-125"
+                src="/a2.png"
+                fill
+              />
+            </SwiperSlide>
+            <SwiperSlide className="group">
+              <Image
+                className="h-10 group-hover:brightness-125"
+                src="/a1.png"
+                fill
+              />
+            </SwiperSlide>
+            <SwiperSlide className="group">
+              <Image
+                className="h-10 group-hover:brightness-125"
+                src="/a3.png"
+                fill
+              />
+            </SwiperSlide>
+            <SwiperSlide className="group">
+              <Image
+                className="h-10 group-hover:brightness-125"
+                src="/a4.png"
+                fill
+              />
+            </SwiperSlide>
+          </div>
+        </Swiper>
+        <div className="carousel-buttons z-30">
+          <button className="carousel-button cursor-pointer carousel-button-next">
+            Right
+          </button>
+        </div>
+      </div>
+      <div className="text-center">
+        <div className="swiper-pagination"></div>
+      </div>
+    </div>
+  );
+};
 
+export default CarousalElem;
 
-    // "use client";
-    // import React, { useRef, useState } from "react";
-    // import { Swiper, SwiperSlide } from "swiper/react";
-    // import { EffectCoverflow, Pagination } from "swiper/modules";
+// "use client";
+// import React, { useRef, useState } from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { EffectCoverflow, Pagination } from "swiper/modules";
 
-    // // Import Swiper styles
-    // import "swiper/css";
-    // import "swiper/css/effect-coverflow";
-    // import "swiper/css/pagination";
+// // Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/effect-coverflow";
+// import "swiper/css/pagination";
 
-    // import "./caurosal.css";
+// import "./caurosal.css";
 
-    // const CarousalElem = () => {
-    //   return (
-    //     <div className="flex">
-    //     <button>Left</button>
-    //       <Swiper
-    //         effect={"coverflow"}
-    //         grabCursor={true}
-    //         centeredSlides={true}
-    //         slidesPerView={3}
-    //         coverflowEffect={{
-    //           rotate: 50,
-    //           stretch: 0,
-    //           depth: 100,
-    //           modifier: 1,
-    //           slideShadows: true,
-    //         }}
-    //         pagination={true}
-    //         modules={[EffectCoverflow, Pagination]}
-    //         className="mySwiper"
-    //         initialSlide={3}
-    //       >
-    //         <SwiperSlide>
-    //           <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-    //         </SwiperSlide>
-    //         <SwiperSlide>
-    //           <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-    //         </SwiperSlide>
-    //         <SwiperSlide>
-    //           <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-    //         </SwiperSlide>
-    //         <SwiperSlide>
-    //           <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-    //         </SwiperSlide>
-    //         <SwiperSlide>
-    //           <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-    //         </SwiperSlide>
-    //         <SwiperSlide>
-    //           <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-    //         </SwiperSlide>
-    //       </Swiper>
-    // <button>Right</button>
-    //     </div>
-    //   );
-    // };
+// const CarousalElem = () => {
+//   return (
+//     <div className="flex">
+//     <button>Left</button>
+//       <Swiper
+//         effect={"coverflow"}
+//         grabCursor={true}
+//         centeredSlides={true}
+//         slidesPerView={3}
+//         coverflowEffect={{
+//           rotate: 50,
+//           stretch: 0,
+//           depth: 100,
+//           modifier: 1,
+//           slideShadows: true,
+//         }}
+//         pagination={true}
+//         modules={[EffectCoverflow, Pagination]}
+//         className="mySwiper"
+//         initialSlide={3}
+//       >
+//         <SwiperSlide>
+//           <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+//         </SwiperSlide>
+//         <SwiperSlide>
+//           <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+//         </SwiperSlide>
+//       </Swiper>
+// <button>Right</button>
+//     </div>
+//   );
+// };
 
-    // export default CarousalElem;
+// export default CarousalElem;
