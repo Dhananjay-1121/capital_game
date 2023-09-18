85f3b7a9513cc4cdfc5eace9c1271d7a8fe1f826
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-18 06:44:24 +0530

    section 3 arrow and boxes matched

--- components/home/Home3.jsx
+++ components/home/Home3.jsx
@@ -33,16 +33,99 @@ const Home3 = () => {
                 </p>
               </div>
               <div className="mr-28  -mt-[200px]">
-                  <Image className=" relative top-[226px] left-6 " src="/p1.png" height={315} width={300} /> 
-                <div className="bg-gradient-to-bl from-[#069F67] rounded-l-xl rounded-r-xl to-[#33CB53] w-[350px] h-[205px] "></div> 
-                  <Image className="relative bottom-[350px] right-[75px] opacity-60" src="/p.png" height={520} width={495} />
+                <Image
+                  className=" relative top-[226px] left-6 "
+                  src="/p1.png"
+                  height={315}
+                  width={300}
+                />
+                <div className="bg-gradient-to-bl from-[#069F67] rounded-l-xl rounded-r-xl to-[#33CB53] w-[350px] h-[205px] "></div>
+                <Image
+                  className="relative bottom-[350px] right-[75px] opacity-60"
+                  src="/p.png"
+                  height={520}
+                  width={495}
+                />
               </div>
             </div>
-            <div className="flex justify-between -mt-[200px]" id="two">
-              <div className="ml-28  -mt-[200px]">
-                  <Image className=" relative top-[226px] left-6 " src="/p1.png" height={315} width={300} /> 
-                <div className="bg-gradient-to-bl from-[#069F67] rounded-l-xl rounded-r-xl to-[#33CB53] w-[350px] h-[205px] "></div> 
-                  <Image className="relative bottom-[350px] right-[75px] opacity-60" src="/p.png" height={520} width={495} />
+            <div className="relative bottom-[535px] h-[303px] ml-[235px] mr-[320px] xl:mr-[420px] w-auto">
+              <Image src="/pleft.png" fill />
+            </div>
+            <div className="flex justify-between -mt-[610px]" id="two">
+              <div className="ml-16 -mt-[200px]">
+                <Image
+                  className=" relative top-[226px] left-6 "
+                  src="/p1.png"
+                  height={315}
+                  width={300}
+                />
+                <div className="bg-gradient-to-bl from-[#069F67] rounded-l-xl rounded-r-xl to-[#33CB53] w-[350px] h-[205px] "></div>
+                <Image
+                  className="relative bottom-[350px] right-[75px] opacity-60"
+                  src="/p.png"
+                  height={520}
+                  width={495}
+                />
+              </div>
+              <div className=" text-left mr-20">
+                <h2 className=" leading-tight w-[600px]  text-white text-[48px] ">
+                  Sign up & Build your <br /> Capital Empire
+                </h2>
+                <p className=" w-[530px] text-white -mt-5 font-quicksand leading-normal text-[20px]">
+                  Mr Capital will guide you to several amazing adventures
+                  throughout this wonderful journey. You would be able to create
+                  the desired portfolio from your in-hand cash.
+                </p>
+              </div>
+            </div>
+            <div className="relative bottom-[535px] h-[303px] ml-[235px] mr-[320px] xl:mr-[380px] w-auto">
+              <Image src="/pright.png" fill />
+            </div>
+            <div className="flex justify-between -mt-[610px]" id="two">
+              <div className=" text-left ml-20">
+                <h2 className=" leading-tight w-[600px]  text-white text-[48px] ">
+                  Sign up & Build your <br /> Capital Empire
+                </h2>
+                <p className=" w-[530px] text-white -mt-5 font-quicksand leading-normal text-[20px]">
+                  Mr Capital will guide you to several amazing adventures
+                  throughout this wonderful journey. You would be able to create
+                  the desired portfolio from your in-hand cash.
+                </p>
+              </div>
+              <div className="mr-16 -mt-[200px]">
+                <Image
+                  className=" relative top-[226px] left-6 "
+                  src="/p1.png"
+                  height={315}
+                  width={300}
+                />
+                <div className="bg-gradient-to-bl from-[#069F67] rounded-l-xl rounded-r-xl to-[#33CB53] w-[350px] h-[205px] "></div>
+                <Image
+                  className="relative bottom-[350px] right-[75px] opacity-60"
+                  src="/p.png"
+                  height={520}
+                  width={495}
+                />
+              </div>
+            </div>
+            <div className="relative bottom-[535px] h-[303px] ml-[235px] mr-[320px] xl:mr-[380px] w-auto">
+              <Image src="/pleft.png" fill />
+            </div>
+            <div className="flex justify-between -mt-[610px]" id="two">
+              <div className="ml-16 -mt-[200px]">
+                <Image
+                  className=" relative top-[226px] left-6 "
+                  src="/p1.png"
+                  height={315}
+                  width={300}
+                />
+                <div className="bg-gradient-to-bl from-[#069F67] rounded-l-xl rounded-r-xl to-[#33CB53] w-[350px] h-[205px] "></div>
+                <Image
+                  className="relative bottom-[350px] right-[75px] opacity-60"
+                  src="/p.png"
+                  height={520}
+                  width={495}
+                />
               </div>
               <div className=" text-left mr-20">
                 <h2 className=" leading-tight w-[600px]  text-white text-[48px] ">
